48c759b85b2a7fbd4bc0bac66c580f49b7dabf86
gwq <gwq5210@qq.com>
2014-12-02 00:52:05 +0800

    change file sim.sh

--- core/sim/sim.sh
+++ core/sim/sim.sh
@@ -4,11 +4,10 @@ EXTENSION=`echo "$1" | cut -d'.' -f2`
 for i in ../data/$2/ac/*.$EXTENSION
 do 
 	sim=`/usr/bin/sim_$EXTENSION -p $1 $i |grep ^$1|awk '{print $4}'`
-	if [ ! -z $sim ] && [ $sim -gt 80 ]
+	if [ ! -z $sim ] && [ $sim -gt 50 ]
 	then 
 		sim_s_id=`basename $i`
-		echo "$sim $sim_s_id" >sim
-		exit $sim
+		echo "$sim $sim_s_id" >> sim
 	fi
 done
 exit 0;
