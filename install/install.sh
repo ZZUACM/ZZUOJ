414b3e25cd3d1206562b2f87cd9d3cc842eebb7c
gwq <gwq5210@qq.com>
2015-01-12 18:52:19 +0800

    delete files

--- install/install.sh
+++ install/install.sh
@@ -19,43 +19,13 @@ sudo yum -y update
 sudo yum -y install php httpd php-mysql mysql-server php-xml php-gd gcc-c++  mysql-devel php-mbstring glibc-static flex
 sudo /etc/init.d/mysqld start
 
-#create user and homedir
-sudo  /usr/sbin/useradd -m -u 1536 judge
-
-
-
-#compile and install the core
-cd ../core/
-sudo ./make.sh
-cd ../install/
 #install web and db
-sudo cp -R ../web $WEBBASE/JudgeOnline
-sudo chmod -R 771 $WEBBASE/JudgeOnline
-sudo chown -R $APACHEUSER $WEBBASE/JudgeOnline
+sudo cp -R ../web $WEBBASE
+sudo chmod -R 771 $WEBBASE
+sudo chown -R $APACHEUSER $WEBBASE
 sudo mysql -h localhost -u$DBUSER -p$DBPASS < db.sql
 sudo mysql -h localhost -u$DBUSER -p$DBPASS < create_cha.sql
+sudo mysql -h localhost -u$DBUSER -p$DBPASS < create_vjudge.sql
 
-#create work dir set default conf
-sudo    mkdir /home/judge
-sudo    mkdir /home/judge/etc
-sudo    mkdir /home/judge/data
-sudo    mkdir /home/judge/log
-sudo    mkdir /home/judge/run0
-sudo    mkdir /home/judge/run1
-sudo    mkdir /home/judge/run2
-sudo    mkdir /home/judge/run3
-sudo cp java0.policy  judge.conf /home/judge/etc
-sudo chown -R judge /home/judge
-sudo chgrp -R $APACHEUSER /home/judge/data
-sudo chgrp -R root /home/judge/etc /home/judge/run?
-sudo chmod 775 /home/judge /home/judge/data /home/judge/etc /home/judge/run?
-
-#boot up judged
-sudo cp judged /etc/init.d/judged
-sudo chmod +x  /etc/init.d/judged
-sudo ln -s /etc/init.d/judged /etc/rc3.d/S93judged
-sudo ln -s /etc/init.d/judged /etc/rc2.d/S93judged
-sudo /etc/init.d/judged start
 sudo /etc/init.d/apache2 restart
 sudo /etc/init.d/httpd restart
-
