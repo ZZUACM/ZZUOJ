80e3fa8107e00e22700ac5f90dfa9f699e63cc75
gwq <gwq5210@qq.com>
2015-01-16 10:51:12 +0800

    change backup.sh

--- install/backup.sh
+++ install/backup.sh
@@ -2,6 +2,6 @@
 DBUSER=root
 DBPASS=1234
 DBHOST=127.0.0.1
-mysqldump -u$DBUSER -p$DBPASS -x jol > jol.sql
-mysqldump -u$DBUSER -p$DBPASS -x phpmyadmin > phpmyadmin.sql
-mysqldump -u$DBUSER -p$DBPASS -x sign_up_students > sign_up_students.sql
+mysqldump -h$DBHOST -u$DBUSER -p$DBPASS -x jol > jol.sql
+mysqldump -h$DBHOST -u$DBUSER -p$DBPASS -x phpmyadmin > phpmyadmin.sql
+mysqldump -h$DBHOST -u$DBUSER -p$DBPASS -x sign_up_students > sign_up_students.sql
